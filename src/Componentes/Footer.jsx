89254d19f5e09e5951d8e712b7e4ef9d00ed1e0b
Juan Manuel <114026096+JuanManuel1970@users.noreply.github.com>
2023-07-13 08:23:27 -0300

    dep12

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
         <div className="row">
           <div className="col-md-4">
             <h5>Encontranos en:</h5>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.7417512146235!2d-58.36567972500077!3d-34.66122436053622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a3335032a3a871%3A0xacf3b1f8f91dff4c!2sBodynfinit%20Gym!5e0!3m2!1ses-419!2sar!4v1688984302427!5m2!1ses-419!2sar" width="300" height="225" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            <iframe title="Mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.7417512146235!2d-58.36567972500077!3d-34.66122436053622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a3335032a3a871%3A0xacf3b1f8f91dff4c!2sBodynfinit%20Gym!5e0!3m2!1ses-419!2sar!4v1688984302427!5m2!1ses-419!2sar" width="300"  height="225" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
 
           <div className="col-md-4">
--- src/exports/images.js
+++
@@ -1,5 +0,0 @@
-import  Image1  from "../assets/1.jpg"
-import  Image2  from "../assets/2.jpg"
-import  Image3  from "../assets/3.jpg"
-
-export default [Image1, Image2,Image3 ]
